2226a91bef88fb283a939935d78199996e7dfc60
roshanamjad <65924851+roshanamjad@users.noreply.github.com>
2022-02-14 15:32:25 +0500

    my first commit

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai')
+
+const app = new Clarifai.App({
+      apiKey: 'e905ea17f3b644c688f3de64f58937ae'
+      })
+
+const handleApiCall = (req, res) => {
+	app.models
+      	.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+      	.then(data => {
+      		res.json(data)
+      	})
+      	.catch(err => res.status(400).json("unable to connect with Api"))
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('Counting Error'));
+}
+
+module.exports = {
+	handleImage
+	handleApiCall
+}
---
+++ controllers/signin.js
@@ -0,0 +1,28 @@
+const handleSignin =(db, bcrypt) => (req, res) => {
+	const { email, password} = req.body;
+	if (!email || !password) {
+		res.status(400).json("incorrect form submission")
+	}
+	db.select('email', 'hash').from('login')
+		.where('email', '=', email)
+		.then(data => {
+			const isValid = bcrypt.compareSync(password, data[0].hash);
+			console.log(isValid)
+			if (isValid) {
+				return db.select('*').from('users')
+				.where('email', '=', email)
+				.then(user => {
+					res.json(user[0])
+				})
+				.catch(err => res.status(400).json('unable to get user'))
+			}
+			else{
+				res.status(400).json('unable');
+			}
+		})
+	.catch(err => res.status(400).json('Unable to connect wrong Credntials'));
+}
+
+module.exports = {
+	handleSignin: handleSignin
+};
