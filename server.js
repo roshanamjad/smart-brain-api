473aa6335f6f0aaf71df4000fb679777c0a9b046
roshanamjad <65924851+roshanamjad@users.noreply.github.com>
2022-03-06 15:48:09 +0500

    adding con

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-colorful-0766',
-    user : 'postgres',
-    password : '',
-    database : 'smartbrain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
